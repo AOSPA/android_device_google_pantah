3a4d99ed41ae1ef5cf7d2c4b4205c812e8e86456
Chris Crump <chriscrawford893@gmail.com>
2022-10-21 00:46:43 -0400

    pantah: Add prop imitation configuration
    - Adds the neccessary overlays so prop imitation can send the right fingerprint and model to GMS
    
    Change-Id: Icbf6a33e7a7075fdcf49fb8598383b71f8a4d46f

--- device-aospa.mk
+++ device-aospa.mk
@@ -13,3 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Overlays
+PRODUCT_PACKAGES += \
+    AOSPAFrameworksOverlayC10 \
+    AOSPAFrameworksOverlayP10
